80109e00972c888611b50a7d7b9ad02320418c84
sanjusahu2002 <132829320+sanjusahu2002@users.noreply.github.com>
2024-09-21 17:22:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	if(year%4==0 || year%400==0){
+		document.write(year ," ",366);
+		
+	}
+	else{
+		document.write(year," ",365);
+	}
+}
+daysOfAYear(1700);
